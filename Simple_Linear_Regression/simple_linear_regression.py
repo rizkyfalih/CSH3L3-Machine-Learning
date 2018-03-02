7b37fe363fa846102f3a9fdfbd86b667d73a63bc
Rizky <rizkyfalihpridyandhika@ymail.com>
2018-03-03 06:41:33 +0700

    linear regression finished

--- Simple_Linear_Regression/simple_linear_regression.py
+++ Simple_Linear_Regression/simple_linear_regression.py
@@ -25,3 +25,22 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
+
+# Predicting the Test set result
+y_pred = regressor.predict(x_test)
+
+# Visualising the Training set result
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary VS Experience [Training Set]')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set result
+plt.scatter(x_test, y_test, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary VS Experience [Training Set]')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
