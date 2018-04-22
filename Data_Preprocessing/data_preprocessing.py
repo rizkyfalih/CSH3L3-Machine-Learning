cff01b4cfc159ead9930f20045251ec05d37df8c
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-04-22 15:40:51 +0700

    updated

--- Data_Preprocessing/data_preprocessing.py
+++ Data_Preprocessing/data_preprocessing.py
@@ -17,12 +17,13 @@ imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # Encoding categorical data
-# Encoding the Independent Variable
+# Encoding the Independent Variable (String to matrix)
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x = LabelEncoder()
 x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 x = onehotencoder.fit_transform(x).toarray()
+
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_x.fit_transform(y)
--- Naive_Bayes/naive_bayes.py
+++ Naive_Bayes/naive_bayes.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
+X = dataset.iloc[:, [1, 2]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
